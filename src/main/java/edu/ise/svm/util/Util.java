c17a9d4ee6c53adc7d398c24f2476b98cca86678
Vibhatha Abeykoon <vibhatha@gmail.com>
2018-03-09 12:27:29 -0500

    new util function added

--- src/main/java/edu/ise/svm/util/Util.java
+++ src/main/java/edu/ise/svm/util/Util.java
@@ -1,11 +1,15 @@
 package edu.ise.svm.util;
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
+
 
 /**
  * Created by vibhatha on 2/27/18.
  */
 public class Util {
+
+    private final static Logger LOG = Logger.getLogger(Util.class.getName());
 
     static{
         System.setProperty("java.util.logging.SimpleFormatter.format",
@@ -29,6 +33,20 @@ public class Util {
         else{
             return null;
         }
+    }
+
+    public static String [] optArgs(String [] args){
+        if(args!=null && args.length==5){
+            LOG.info("Source Folder : "+args[0]);
+            LOG.info("Train X: "+args[1]);
+            LOG.info("Train Y : "+args[2]);
+            LOG.info("Test X : "+args[3]);
+            LOG.info("Test Y : "+args[4]);
+        }else{
+            LOG.info("Usage java -Xms30072m -cp target/svm-ise.jar edu.ise.svm.Main <source-folder> " +
+                    " <train-X> <train-Y> <test-X> <test-Y>");
+        }
+        return args;
     }
 
 
