5ee7fdd94dbfb46d2cb77c413467978bcb7d0acc
Vibhatha Abeykoon <vibhatha@gmail.com>
2018-04-13 12:27:18 -0400

    new:deV: fix error

--- src/main/java/edu/ise/svm/smo/newton/sdsm/SVM.java
+++ src/main/java/edu/ise/svm/smo/newton/sdsm/SVM.java
@@ -405,7 +405,7 @@ public class SVM {
             iteration++;
             String minibatchRecord = "";
             minibatchRecord = EXPERIMENT_NAME+","+MINIBATCH_SIZE+","+iteration+","+current_error+","+gamma+","+C+","+KERNEL+"\n";
-            MINIBATCH_LOG_PATH = MINIBATCH_LOG_PATH+EXPERIMENT_NAME;
+            MINIBATCH_LOG_PATH = "stats/minibath/"+EXPERIMENT_NAME;
             LOG.info("Minibatch Log : "+MINIBATCH_LOG_PATH);
             UtilDynamicSingle.appendLogs(MINIBATCH_LOG_PATH, minibatchRecord);
         }
