eb6c9f984286160dcfd22f63f5bd967c182bf419
Vibhatha Abeykoon <vibhatha@gmail.com>
2018-04-15 08:58:17 -0400

    new:dev: new experiment script added

---
+++ run-ddmsvm-minibatch.sh
@@ -0,0 +1,9 @@
+expid=$1
+partitions=$2
+Cval=$3
+gamma=$4
+kernel=$5
+baseacc=$6
+datasource=$7
+#echo "java -Xms30072m -cp target/svm-ddmsvm-0.1.jar edu.ise.svm.experiments.dynamic.checkacc.BulkMDMMTraining /home/vibhatha/data/libsvm/webspam/ webspam_isesvm_train_x_bin.1 webspam_isesvm_train_y.1.bin webspam_isesvm_test_x_bin.1 webspam_isesvm_test_y.1.bin model/single/webspam/${expid} ${partitions} ${Cval} ${gamma} ${kernel}"
+java -Xmx2G -Xms2G -cp target/svm-ise.jar edu.ise.svm.smo.newton.sdsm.SVM ~/data/libsvm/${datasource}/ ${datasource}_isesvm_train_x_bin.1 ${datasource}_isesvm_train_y.1.bin ${datasource}_isesvm_test_x_bin.1 ${datasource}_isesvm_test_y.1.bin model/single/${datasource}/${expid} ${partitions} ${Cval} ${gamma} ${kernel} ${baseacc} ${datasource}
